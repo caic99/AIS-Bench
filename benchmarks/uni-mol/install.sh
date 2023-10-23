16ed2288b6ad9b6f2209b750d4cae2fbdb0b6fe0
Chun Cai <amoycaic@gmail.com>
2023-10-23 16:24:10 +0800

    update pytorch install command

--- benchmarks/uni-mol/install.sh
+++ benchmarks/uni-mol/install.sh
@@ -1,12 +1,9 @@
-
-conda create -n unimol pytorch torchvision torchaudio -c pytorch
+conda create -n unimol pytorch torchvision torchaudio -c pytorch -c nvidia
 conda activate unimol
 # https://github.com/dptech-corp/Uni-Core#installation
 pip install git+https://github.com/dptech-corp/Uni-Core.git
 
 # https://github.com/dptech-corp/Uni-Mol/tree/main/unimol#dependencies
 git clone https://github.com/dptech-corp/Uni-Mol.git
-pip install ./Uni-Mol
-pip install rdkit-pypi==2022.9.3
-pip install ./unimol_tools
+pip install ./Uni-Mol/unimol ./Uni-Mol/unimol_tools rdkit-pypi==2022.9.3
 wget -c https://github.com/dptech-corp/Uni-Mol/releases/download/v0.1/mol_pre_all_h_220816.pt -O ~/miniconda3/lib/python3.11/site-packages/unimol_tools/weights/mol_pre_all_h_220816.pt
