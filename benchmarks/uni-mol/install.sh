6e4143b6876eec4aa82c6aa43d7176ce992ba7f7
Chun Cai <amoycaic@gmail.com>
2023-10-23 16:29:00 +0800

    fix model weight download path

--- benchmarks/uni-mol/install.sh
+++ benchmarks/uni-mol/install.sh
@@ -6,4 +6,6 @@ pip install git+https://github.com/dptech-corp/Uni-Core.git
 # https://github.com/dptech-corp/Uni-Mol/tree/main/unimol#dependencies
 git clone https://github.com/dptech-corp/Uni-Mol.git
 pip install ./Uni-Mol/unimol ./Uni-Mol/unimol_tools rdkit-pypi==2022.9.3
-wget -c https://github.com/dptech-corp/Uni-Mol/releases/download/v0.1/mol_pre_all_h_220816.pt -O ~/miniconda3/lib/python3.11/site-packages/unimol_tools/weights/mol_pre_all_h_220816.pt
+
+cd `python -c "import unimol_tools; print(unimol_tools.__path__[0])"`/weights
+wget -c https://github.com/dptech-corp/Uni-Mol/releases/download/v0.1/mol_pre_all_h_220816.pt
