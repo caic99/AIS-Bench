2e91fee735161fc2a19e8b994a5abd60e3156e90
caic99 <amoycaic@gmail.com>
2023-10-25 11:51:42 +0800

    Update uni-mol install command

--- benchmarks/uni-mol/install.sh
+++ benchmarks/uni-mol/install.sh
@@ -1,11 +1,14 @@
 conda create -n unimol pytorch torchvision torchaudio -c pytorch -c nvidia
 conda activate unimol
-# https://github.com/dptech-corp/Uni-Core#installation
-pip install git+https://github.com/dptech-corp/Uni-Core.git
 
 # https://github.com/dptech-corp/Uni-Mol/tree/main/unimol#dependencies
-git clone https://github.com/dptech-corp/Uni-Mol.git
-pip install ./Uni-Mol/unimol ./Uni-Mol/unimol_tools rdkit-pypi==2022.9.3
+# https://github.com/dptech-corp/Uni-Core#installation
+# Building wheel for unicore may take minutes
+pip install \
+	git+https://github.com/dptech-corp/Uni-Core.git \
+	git+https://github.com/dptech-corp/Uni-Mol.git#subdirectory=unimol \
+	git+https://github.com/dptech-corp/Uni-Mol.git#subdirectory=unimol_tools \
+	rdkit-pypi==2022.9.3
 
-cd `python -c "import unimol_tools; print(unimol_tools.__path__[0])"`/weights
-wget -c https://github.com/dptech-corp/Uni-Mol/releases/download/v0.1/mol_pre_all_h_220816.pt
+wget -c https://github.com/dptech-corp/Uni-Mol/releases/download/v0.1/mol_pre_all_h_220816.pt \
+	-P `python -c "import unimol_tools; print(unimol_tools.__path__[0])"`/weights
