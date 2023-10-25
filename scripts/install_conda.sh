3088f21bca1177fba67b35dfe2092c8df62e310c
caic99 <amoycaic@gmail.com>
2023-10-25 11:52:40 +0800

    set libmamba as the default solver when installing conda

--- scripts/install_conda.sh
+++ scripts/install_conda.sh
@@ -3,3 +3,8 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b
 ~/miniconda3/bin/conda init
 source ~/.bashrc
+
+# Recommended: set libmamba as the default solver
+# https://www.anaconda.com/blog/conda-is-fast-now
+conda install -n base conda-libmamba-solver
+conda config --set solver libmamba
