d1f817756399a5e9d88ead4ca59cec21962ff642
Chun Cai <amoycaic@gmail.com>
2023-10-25 11:28:32 +0800

    add cuda repo mirror address

--- scripts/install_cuda.sh
+++ scripts/install_cuda.sh
@@ -2,6 +2,7 @@
 
 # https://developer.nvidia.com/cuda-downloads
 # Install CUDA for ubuntu 22.04
+# mirror: https://mirrors.aliyun.com/nvidia-cuda/ubuntu2204
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
