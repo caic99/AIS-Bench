2fe9c26eaf99d8052d354b8b479013d6adac3d38
dingzhaohan <dingzhaohan@pku.edu.cn>
2023-12-18 15:21:59 +0800

    fix mpirun parameters

--- benchmarks/abacus+deepks/water_scan0/run.sh
+++ benchmarks/abacus+deepks/water_scan0/run.sh
@@ -1,12 +1,18 @@
-wget -c https://hpc-profiling-example.oss-cn-beijing.aliyuncs.com/example/abacus/water_scan0_test.zip
-unzip -f water_scan0_test.zip
-cd water_scan0_test
-ln -sf INPUT_deepks INPUT
+#!/bin/bash -ex
+if [ ! -d "water_scan0_test" ]; then
+	wget -c https://hpc-profiling-example.oss-cn-beijing.aliyuncs.com/example/abacus/water_scan0_test.zip
+	unzip water_scan0_test.zip
+	cd water_scan0_test
+	ln -sf INPUT_deepks INPUT
+	cd ..
+fi
 
+cd water_scan0_test
 source activate abacus
 np=`nproc`
 nt=1
-OMP_NUM_THREADS=nt mpirun -n np abacus # Ref: 486.57 s
+OMP_NUM_THREADS=$nt mpirun -n $np abacus # Ref: 486.57 s
 
 cat OUT.ABACUS/running_scf.log | grep -i final_etot
 ## !FINAL_ETOT_IS -29927.1311197787763376 eV
+cd -
