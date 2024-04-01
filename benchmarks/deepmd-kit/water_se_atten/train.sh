e18fe500caf9161348131b14bd5d0439e2ee8589
caic99 <amoycaic@gmail.com>
2024-04-01 13:15:16 +0800

    update script for get number of gpus

--- benchmarks/deepmd-kit/water_se_atten/train.sh
+++ benchmarks/deepmd-kit/water_se_atten/train.sh
@@ -3,7 +3,7 @@ cd ../deepmd-kit/examples/water/se_atten
 
 # TODO: Select a certain batch*step
 # https://docs.deepmodeling.com/projects/deepmd/en/master/train/parallel-training.html#how-to-use
-ngpu=${nvidia-smi --query-gpu=count --format=csv,noheader}
+ngpu=$(nvidia-smi --query-gpu=count --format=csv,noheader)
 horovodrun -np $ngpu dp train --mpi-log=workers input.json
 # dp train input.json
 # DEEPMD rank:0  INFO    wall time: 81872.166 s
