e97d0256257a59a1c8fc72e30763c22bf07b599a
Chun Cai <amoycaic@gmail.com>
2024-08-12 15:23:19 +0800

    Checkout corresponding branch for deepmd-kit v3

--- benchmarks/deepmd-kit-v3/install.sh
+++ benchmarks/deepmd-kit-v3/install.sh
@@ -1,4 +1,4 @@
 conda create -n dp3 deepmd-kit lammps -c conda-forge/label/deepmd-kit_dev -c conda-forge -y
 # deepmd-kit-3.0.0a0|cuda120py311hb63a56f_mpi_mpich_0
 conda activate dp3
-git clone https://github.com/deepmodeling/deepmd-kit.git
+git clone https://github.com/deepmodeling/deepmd-kit.git -b v3.0.0b3
