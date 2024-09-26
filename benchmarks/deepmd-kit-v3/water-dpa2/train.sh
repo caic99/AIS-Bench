0a1c1a0b22cefd860139e6f0268d8c439d38c978
Chun Cai <amoycaic@gmail.com>
2024-09-26 10:43:46 +0800

    change input file path for dp3 b4

--- benchmarks/deepmd-kit-v3/water-dpa2/train.sh
+++ benchmarks/deepmd-kit-v3/water-dpa2/train.sh
@@ -1,7 +1,7 @@
 # Training
 cd ../deepmd-kit/examples/water/dpa2
 # modify `numb_steps` in input[_torch].json for fast dev run
-dp --pt train input_torch.json
+dp --pt train input_torch.json # input_torch_large.json for 3.0.0b4
 
 # Freezing
 # ensure ./checkpoint is present
