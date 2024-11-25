72ea8016d654732019832819ab8675c3c79acd6c
Chun Cai <amoycaic@gmail.com>
2024-11-25 15:11:13 +0800

    Update deepmd v3 to release

--- benchmarks/deepmd-kit-v3/install.sh
+++ benchmarks/deepmd-kit-v3/install.sh
@@ -1,4 +1,4 @@
-conda create -n dp3 deepmd-kit lammps -c conda-forge/label/deepmd-kit_rc -c conda-forge -y
-# deepmd-kit-3.0.0a0|cuda120py311hb63a56f_mpi_mpich_0
+conda create -n dp3 deepmd-kit=3 lammps -c conda-forge -y
+# conda-forge/linux-64::deepmd-kit-3.0.0-cuda120py312he7ec23b_mpi_mpich_0
 conda activate dp3
-git clone https://github.com/deepmodeling/deepmd-kit.git -b v3.0.0b3
+git clone https://github.com/deepmodeling/deepmd-kit.git -b v3.0.0
