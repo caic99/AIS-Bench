43d15190fefe3bafac94729fce3050adf3f4f13c
Chun Cai <amoycaic@gmail.com>
2025-02-26 10:07:06 +0800

    Uni-Mol: pin rdkit version

--- benchmarks/uni-mol/install.sh
+++ benchmarks/uni-mol/install.sh
@@ -8,7 +8,7 @@ pip install \
 	git+https://github.com/dptech-corp/Uni-Core.git \
 	git+https://github.com/dptech-corp/Uni-Mol.git#subdirectory=unimol \
 	git+https://github.com/dptech-corp/Uni-Mol.git#subdirectory=unimol_tools \
-	rdkit
+	rdkit==2023.9.1  # The latest RDKit might has errors on loading training data
 
 wget -c https://github.com/dptech-corp/Uni-Mol/releases/download/v0.1/mol_pre_all_h_220816.pt \
 	-P `python -c "import unimol_tools; print(unimol_tools.__path__[0])"`/weights
